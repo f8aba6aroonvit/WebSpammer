1b362ce2794099288733e6524f5e8e7cc4521bbb
f8aba6aroonvit <aroonvit@gmail.com>
2023-08-18 23:21:46 +0700

    Update Test1.cy.js

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -2,7 +2,9 @@ require('cypress-xpath');
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    cy.xpath('//*[@id="navbar"]/ul[1]/li[3]/a').click()
+    cy.visit('www.youtube.com')
+    cy.get('[id="search-input"]').type('322')
+    cy.xpath('//*[@id="search-icon-legacy"]').click()
+    
   })
 })
