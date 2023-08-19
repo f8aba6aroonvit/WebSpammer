5614e4f92ee9f7f48fc0eed9528ea057c7e8e64e
f8aba6aroonvit <aroonvit@gmail.com>
2023-08-20 04:21:08 +0700

    Update Test1.cy.js

--- cypress/e2e/Test1.cy.js
+++ cypress/e2e/Test1.cy.js
@@ -3,7 +3,13 @@ require('cypress-xpath');
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('www.google.com')
-    cy.get('[jsname="yZiJbe""]').type('322')
+    for(let i = 0 ; i<10 ; i++){
+      cy.get('[jsname="yZiJbe"]').type('f8aba6')
+      cy.get('[class="gNO89b"]').eq(0).click()
+      cy.visit('www.google.com')
+    }
+
+   
     
     
   })
